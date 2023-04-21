6e8609b04eca2657dac9f9b9680edf6bded1e926
Dikhtiar-Tania96 <chygynovatania96@gmail.com>
2023-04-21 09:08:48 +0300

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
 
+let timerInterval = null;
+
+btnStart.addEventListener('click', onClickStart);
+btnStop.addEventListener('click', onClickStop);
+
+function onClickStart(evt) {
+  btnStart.disabled = true;
+  btnStop.disables = false;
+
+  bgColor();
+  timerInterval = setInterval(bgColor, 1000);
+}
+
+function onClickStop(evt) {
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+  clearInterval(timerInterval);
+}
+
+function bgColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
